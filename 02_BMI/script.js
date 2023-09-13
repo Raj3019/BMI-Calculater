e199ed2e48d70e508573491af61258955d4c7990
Raj3019 <86660451+Raj3019@users.noreply.github.com>
2023-09-13 19:55:43 +0530

    Update script.js

--- 02_BMI/script.js
+++ 02_BMI/script.js
@@ -3,9 +3,9 @@ const form = document.querySelector('form')
 form.addEventListener('submit', function(e){
   e.preventDefault()
 
-  const height = parseInt(doucument.querySelector('#height').value)
-  const weight = parseInt(doucument.querySelector('#weight').value)
-  const results = doucument.querySelector('#results')
+  const height = parseInt(document.querySelector('#height').value)
+  const weight = parseInt(document.querySelector('#weight').value)
+  const results = document.querySelector('#results')
   
   if (height === ''|| height < 0 || isNaN(height)){
     results.innerHTML = `Please Give a valid Height ${height}.`
